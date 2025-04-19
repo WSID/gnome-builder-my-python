45dacd159f6b3c46d16623afde731cbd4635d127
Wissle <jongsome@gmail.com>
2025-04-19 11:32:37 +0900

    feat: python-venv: Added skeletal plugin.

---
+++ plugins/python-venv/python-venv-plugin.c
@@ -0,0 +1,13 @@
+/**
+ * python-venv-plugin.c
+ */
+
+#define G_LOG_DOMAIN "python-venv"
+
+#include <gmodule.h>
+#include <libpeas.h>
+
+G_MODULE_EXPORT void peas_register_types (PeasObjectModule *module);
+
+G_MODULE_EXPORT void peas_register_types (PeasObjectModule *module) {
+}
